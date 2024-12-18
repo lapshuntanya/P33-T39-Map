2cc85e96e8b41d1dbd788bd1a37e8f5133bf505a
lapshuntanya <lapshun.tatiana@gmail.com>
2024-12-18 17:15:10 +0200

    first task

--- P33-T39-Map/P33-T39-Map.cpp
+++ P33-T39-Map/P33-T39-Map.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <string>
+#include <map>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+     /*Дані рядки, що містять слова.
+     Створити map<string, int>, 
+     ключем у якого є слово,
+     а значенням - кількість повторень.  */
+
+    map<string, int> words; //root = nullptr
+    //key (const, sort) = string => word 
+    //value = int  => amount
+
+    string user;
+    while (user != "stop") {
+        cout << "Input a word: ";
+        cin >> user;
+        if (user == "stop") break;
+
+        if (words.find(user) == words.end()) {
+            //Не знайшли вузол з заданим ключем
+           // words.insert(pair<string, int>(user, 1));
+            words[user] = 1;
+        }
+        else {
+            words[user]++;
+        }
+    }
+    cout << "--------------------------\n";
+    for (auto& item: words)
+    {
+        cout << item.first << " " << item.second << endl;
+    }
+
 }
 
